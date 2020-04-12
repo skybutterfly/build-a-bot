07c19587333c01581af16152cbe2fb114ad2b29e
Adrijana Posedel <skybutterfly@Adrijanas-MacBook-Pro.local>
2020-04-12 16:23:53 +0100

    route params and props params

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+import PartInfo from '../parts/PartInfo.vue';
 
 Vue.use(Router);
 
@@ -15,5 +16,10 @@ export default new Router({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
+    props: true,
   }],
 });
